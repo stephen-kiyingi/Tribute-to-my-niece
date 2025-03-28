02d92199e4ed49c426d955147c79b809bd10cf23
stephen-kiyingi <steverone1989@gmail.com>
2025-03-27 22:15:01 -0400

    Add files via upload

---
+++ js/site.js
@@ -0,0 +1,11 @@
+﻿
+<script>
+        // JavaScript to trigger the fade-in and move-up animation
+    window.onload = function() {
+            const title = document.querySelector('h1');
+    title.style.opacity = 1; // Make the title visible
+    title.style.transform = 'translateY(0)'; // Move it to its original position
+        };
+</script>
+
+
